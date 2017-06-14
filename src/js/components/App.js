4eca565e8db44bab32b78d6399076770b8cab72f
Hristo Tanev <hristotanev7@gmail.com>
2017-06-14 18:59:27 +0100

    Everything is done regarding the display of data. Sorting should be next to implement.

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -16,24 +16,26 @@ export default class App extends React.Component {
 
   render() {
     const users = this.props.campers.users
-    const Campers = users.map((user) => {
+    const Campers = users.map((user, i) => {
       return (<tr>
-                <td>{user.username}</td>
-                <td>{user.recent}</td>
-                <td>{user.alltime}</td>
+                <td>{i + 1}</td>
+                <td><img class="profileimg" src={user.img} />&nbsp;{user.username}</td>
+                <td class="adjust">{user.recent}</td>
+                <td class="adjust">{user.alltime}</td>
               </tr>)
     })
 
     return (
       <div class="container">
         <div class="panel panel-default">
-          <div class="panel-heading">Leaderboard</div>
+          <div class="panel-heading"><strong>Leaderboard</strong></div>
           <table class="table">
             <thead>
               <tr>
+                <th>#</th>
                 <th>Camper name</th>
-                <th>Points in past 30 days</th>
-                <th>All time points</th>
+                <th class="adjust">Points in past 30 days</th>
+                <th class="adjust">All time points</th>
               </tr>
             </thead>
             <tbody>
