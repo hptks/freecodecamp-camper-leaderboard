d3c76827f2ee49e8bd91e4050348a2e6453606a4
Hristo Tanev <hristotanev7@gmail.com>
2017-06-14 15:31:54 +0100

    Sceleton of the app is done.

---
+++ src/js/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class App extends Reatc.Component {
+  render() {
+    return (
+      <h2>Hi there</h2>
+    )
+  }
+}
---
+++ src/js/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux'
+
+import campers from './camperReducer'
+
+export default combineReducers({
+  campers
+})
